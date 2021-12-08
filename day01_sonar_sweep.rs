f323974da9a707b605ee166c196b5931f8dc98bc
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-12-08 22:06:13 +0100

    Solve first challenge - part 1

--- day01_sonar_sweep.rs
+++ day01_sonar_sweep.rs
@@ -1,13 +1,29 @@
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{self, BufRead};
+use std::path::Path;
 
-fn main() -> io::Result<()> {
-    let file = File::open("data/sonar_sweep.txt")?;
-    let reader = BufReader::new(file);
+fn main() {
+    let mut last_seen_depth = 0;
+    // Start counter at -1 since we don't compare the first depth
+    let mut _number_of_increases = -1;
 
-    for line in reader.lines() {
-        println!("{}", line?);
+    if let Ok(lines) = read_lines("data/sonar_sweep.txt") {
+        for line in lines {
+            if let Ok(line) = line {
+                let depth = line.parse::<i32>().unwrap();
+                if depth > last_seen_depth {
+                    _number_of_increases += 1;
+                }
+                last_seen_depth = depth;
+            }
+        }
     }
 
-    Ok(())
+    println!("Number of depth increases: {}", _number_of_increases);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
